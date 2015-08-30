91b0a68729dfc071aa79b3ebb27390969fe314cd
Jessica Le <jessicale90@gmail.com>
2015-08-29 22:39:10 -0500

    ui stuff for tableview

--- TruckFeed/TruckFeed/AppDelegate.swift
+++ TruckFeed/TruckFeed/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().backgroundColor = mainColor
+        UINavigationBar.appearance().barTintColor = mainColor
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         return true
     }
 
--- TruckFeed/TruckTableViewController.swift
+++ TruckFeed/TruckTableViewController.swift
@@ -10,20 +10,17 @@ import UIKit
 
 class TruckTableViewController: UITableViewController {
     
-    let truckList: [Truck] = [Truck(name: "Powered By Fries", type: "Belgian fries", defaultImage: UIImage(named: "powered_by_fries.png")!),
+    let truckList: [Truck] = [Truck(name: "Powered By Fries", type: "Belgian fries", defaultImage: UIImage                      (named: "powered_by_fries.png")!),
                               Truck(name: "Outside Scoop", type: "Dessert", defaultImage: UIImage(named: "the_outside_scoop.jpg")!),
                               Truck(name: "The Spot", type: " Fresh, made-to-order sandwiches", defaultImage: UIImage(named: "the_spot.jpg")!),
-                              Truck(name: "Street Eats DSM", type: "Stuffed sammiches featuring hand-cut fries", defaultImage: UIImage(named: "street_eats_dsm.jpg")!),
+                              Truck(name: "Ferinheit Wood Oven Pizza", type: "Wood Oven Pizza", defaultImage: UIImage(named: "ferinheit_pizza.jpg")!),
                               Truck(name: "Let's Toast", type: "Spanish Tapas", defaultImage: UIImage(named: "lets_toast.jpg")!),
-        
-        Truck(name: "Parlo Pizza", type: "Authentic Neapolitan pizzas", defaultImage: UIImage(named: "parlo_pizza.jpg")!),
+                              Truck(name: "Parlo Pizza", type: "Authentic Neapolitan pizzas", defaultImage: UIImage(named: "parlo_pizza.jpg")!),
                               Truck(name: "Karam's Grill", type: "Mediterranean", defaultImage: UIImage(named: "karams_grill.jpg")!),
-                              Truck(name: "Ferinheit Wood Oven Pizza", type: "Wood Oven Pizza", defaultImage: UIImage(named: "ferinheit_pizza.jpg")!)]
-
+                              Truck(name: "Street Eats DSM", type: "Stuffed sammiches featuring hand-cut fries", defaultImage: UIImage(named: "street_eats_dsm.jpg")!)]
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bringSubviewToFront(view)
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -51,7 +48,11 @@ class TruckTableViewController: UITableViewController {
         
         let truck = truckList[indexPath.row] as Truck
         truckCell.textLabel?.text = truck.name
+        truckCell.textLabel?.textColor = secondaryColor
         truckCell.detailTextLabel?.text = truck.type
+        
+        truckCell.imageView?.frame = CGRectMake(30,30,30,30)
+        truckCell.imageView?.contentMode = UIViewContentMode.ScaleAspectFill
         truckCell.imageView?.image = truck.defaultImage
         return truckCell
     }
