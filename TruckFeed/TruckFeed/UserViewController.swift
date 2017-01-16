fbe70928f6c9101ffe31795cd0a5a010344388d1
Jessica Le <jessicale90@gmail.com>
2017-01-15 18:18:24 -0600

    US31 - Resizing tableviewcells dynamically

--- TruckFeed/TruckFeed/UserViewController.swift
+++ TruckFeed/TruckFeed/UserViewController.swift
@@ -35,9 +35,13 @@ open class UserViewController: UIViewController, UINavigationBarDelegate, UIText
         self.view.isOpaque = false
         self.view.tintColor = mainColor
         dataProvider = AddScheduleDataProvider()
-        tableView!.delegate = self
-        tableView!.dataSource = dataProvider
+        self.tableView!.delegate = self
+        self.tableView!.dataSource = dataProvider
         dataProvider?.tableView = tableView
+        self.tableView!.estimatedRowHeight = 100
+        self.tableView!.rowHeight = UITableViewAutomaticDimension
+        self.tableView!.setNeedsLayout()
+        self.tableView!.layoutIfNeeded()
 
         self.scrollView.delegate = self
         self.hideKeyboard()
@@ -132,9 +136,9 @@ extension UserViewController: UITableViewDelegate
     
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
-        //TODO: dynamically determine cell size based on switch or picker 
-        return 100.0
+        return UITableViewAutomaticDimension
     }
+
 }
 
 
