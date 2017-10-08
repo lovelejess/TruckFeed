3d74c6b4ef6ca81b53e1b3fba1c5b150b7e52420
Jessica Le <jle@pillartechnology.com>
2017-10-07 22:01:40 -0500

    Better loggin messages

--- TruckFeed/TruckFeed/FacebookAPI.swift
+++ TruckFeed/TruckFeed/FacebookAPI.swift
@@ -19,18 +19,20 @@ class FacebookAPI: NSObject {
     }
     
     static func requestFacebookCredentials() -> String {
-        NSLog("retrieveUserAccessInfoFromFBRequest - getting facebook user access info")
+        NSLog("requestFacebookCredentials - getting facebook user access info")
         var fbAccessUserID = String()
-        let request = FBSDKGraphRequest(graphPath:"me", parameters: ["fields": "name, email, friends, id"] , httpMethod: "GET")
+        let request = FBSDKGraphRequest(graphPath:"me", parameters: ["fields": "id,first_name,last_name,email"] , httpMethod: "GET")
         let connection = FBSDKGraphRequestConnection()
         connection.add(request, completionHandler: {
             (connection, result, error) in
             if(error != nil){
-                NSLog("retrieveUserAccessInfoFromFBRequest - Error retrieving fb graph request:  \(error?.localizedDescription)")
+                NSLog("requestFacebookCredentials - Error retrieving fb graph request:  \(error!.localizedDescription)")
             }
             else {
                 if let response = result as AnyObject? {
+                    NSLog("requestFacebookCredentials - response: \(response)")
                     fbAccessUserID = response.value(forKey: "id") as! String
+                    NSLog("requestFacebookCredentials - fbAccessUserId: \(fbAccessUserID)")
                 }
             }
         })
@@ -39,20 +41,21 @@ class FacebookAPI: NSObject {
     }
     
     static func retrieveFBPageIDFromFBRequest(userAccessInfo: String) -> NSArray {
-        NSLog("getFBPageID - getting facebook page id")
+        NSLog("retrieveFBPageIDFromFBRequest - getting facebook page id")
         var facebookPageData = NSArray()
-        let request = FBSDKGraphRequest(graphPath:"\(userAccessInfo)/accounts", parameters: nil , httpMethod: "GET")
+        let request = FBSDKGraphRequest(graphPath:"me/accounts", parameters: ["fields": "data"]  , httpMethod: "GET")
         
         let connection = FBSDKGraphRequestConnection()
         connection.add(request, completionHandler: {
             (connection, result, error) in
             if(error != nil){
-                NSLog("getFBPageID - Error retrieving fb graph request:  \(error?.localizedDescription)")
+                NSLog("retrieveFBPageIDFromFBRequest - Error retrieving fb graph request:  \(error!.localizedDescription)")
             }
             else {
                 if let response = result as AnyObject? {
                     if let data = response.value(forKey: "data") as? NSArray {
                         facebookPageData = data
+                         NSLog("retrieveFBPageIDFromFBRequest - fbAccessUserId: \(facebookPageData)")
                     }
                 }
             }
