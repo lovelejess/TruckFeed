60a1feba2c1b7a392ad7833f2ff85120a4edd2b9
Jessica Le <jessicaduongle@gmail.com>
2017-11-05 20:00:57 -0600

    Add pop up view controller to select and verify food truck

--- TruckFeed/TruckFeed/VerifyTruckOwnerController.swift
+++ TruckFeed/TruckFeed/VerifyTruckOwnerController.swift
@@ -12,7 +12,7 @@ class VerifyTruckOwnerController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let frame = CGRect(x: 0, y: 15, width: self.view.frame.size.width, height: 55)
+        let frame = CGRect(x: 0, y: 15, width: 300, height: 55)
         let leftBarButtonItem = ViewControllerItems.createBarButtonItemWithImage(#selector(self.dismissViewController), frame:CGRect(x: 0, y: 15, width: 20, height: 20), image: UIImage(named: "back_button")!, target: self)
         let rightBarButtonItem = ViewControllerItems.createBarButtonItemWithImage(#selector(self.cancel), frame:CGRect(x: 0, y: 15, width: 15, height: 20), image: UIImage(named: "cancel_button")!, target: self)
         let navigationBar = ViewControllerItems.createNavigationBarWithButtons(frame, title: "Verify Truck Owner", leftBarButton: leftBarButtonItem, rightBarButton: rightBarButtonItem)
