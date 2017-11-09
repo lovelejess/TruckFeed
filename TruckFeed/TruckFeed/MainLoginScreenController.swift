857a37bbd068f5a9bb7cecc830e81801601594dd
Jessica Le <jessicaduongle@gmail.com>
2017-11-08 21:25:05 -0600

    I like the full screen view instead of the popup for verifying truck owner

--- TruckFeed/TruckFeed/MainLoginScreenController.swift
+++ TruckFeed/TruckFeed/MainLoginScreenController.swift
@@ -19,6 +19,7 @@ class MainLoginScreenController: UIViewController, UIPopoverControllerDelegate, 
         self.view.addSubview(ViewControllerItems.createButton("TRUCK OWNER", onClick: #selector(MainLoginScreenController.loginWithFacebook), frame: CGRect(x: 90, y: 500, width: 200, height: 50), target: self))
         self.view.addSubview(ViewControllerItems.createButton("TRUCK CONSUMER", onClick: #selector(MainLoginScreenController.presentTruckFeedController(_:)), frame: CGRect(x: 90, y: 570, width: 200, height: 50), target: self))
     }
+
     
     func loginWithFacebook()
     {
@@ -82,52 +83,11 @@ class MainLoginScreenController: UIViewController, UIPopoverControllerDelegate, 
     }
     
     func presentVerifyTruckOwnerController(_ sender: AnyObject){
-        let popoverContent = self.storyboard!.instantiateViewController(withIdentifier: "VerifyTruckOwnerController") as! VerifyTruckOwnerController
-        popoverContent.modalPresentationStyle = .popover
-        if let popover = popoverContent.popoverPresentationController {
-            popover.sourceView = self.view
-            popover.permittedArrowDirections = UIPopoverArrowDirection.init(rawValue: 0)
-            popover.sourceRect = CGRect(x: self.view.bounds.width/2, y: self.view.bounds.height/2, width: 1, height: 1)
-            popoverContent.preferredContentSize = CGSize(width: 300, height: 400)
-            popover.delegate = self
-        }
-        
-        self.present(popoverContent, animated: true, completion: nil)
-    }
-    
-    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-        
-        return UIModalPresentationStyle.none
-    }
-    
-    
-    func presentVerifyTruckAlertView() {
-        if let _: AnyClass = NSClassFromString("UIAlertController") {
-            let myAlert: UIAlertController = UIAlertController(title: "Verify Food Truck", message: "Select the name of the food truck linked to your Facebook Page account", preferredStyle: .actionSheet)
-            
-            let action1 = UIAlertAction(title: "Action 1", style: .default, handler: { (action) -> Void in
-                print("ACTION 1 selected!")
-                self.presentMasterViewController(self)
+        if let viewController = self.storyboard!.instantiateViewController(withIdentifier: "VerifyTruckOwnerController") as? VerifyTruckOwnerController {
+            self.present(viewController, animated: true, completion:
+                {
+                    NSLog("Presenting Verify Truck Owner Controller")
             })
-            
-            let action2 = UIAlertAction(title: "Action 2", style: .default, handler: { (action) -> Void in
-                print("ACTION 2 selected!")
-                self.presentMasterViewController(self)
-            })
-            
-            let action3 = UIAlertAction(title: "Action 3", style: .default, handler: { (action) -> Void in
-                print("ACTION 3 selected!")
-                self.presentMasterViewController(self)
-            })
-            
-            let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: { (action) -> Void in })
-            myAlert.view.layer.cornerRadius = 25
-            myAlert.addAction(action1)
-            myAlert.addAction(action2)
-            myAlert.addAction(action3)
-            myAlert.addAction(cancel)
-
-            self.present(myAlert, animated: true, completion: nil)
         }
     }
     
